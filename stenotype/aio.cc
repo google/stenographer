43da5ae5d8cf40a41ddf9c7ff7ef5f073b3334fd
Alexis Papadopoulos <papadopa@google.com>
2014-10-27 23:00:07 +0200

    Formatting C++ code with clang-format --style=Google

--- stenotype/aio.cc
+++ stenotype/aio.cc
@@ -14,8 +14,8 @@
 
 #include "aio.h"
 
-#include <errno.h>  // errno
-#include <fcntl.h>  // open()
+#include <errno.h>   // errno
+#include <fcntl.h>   // open()
 #include <unistd.h>  // close()
 #include <libaio.h>
 
@@ -49,9 +49,12 @@ class PWrite {
 class SingleFile {
  public:
   SingleFile(Output* file, const string& dirname, int64_t micros, int fd)
-    : file_(file), fd_(fd), offset_(0), truncate_(-1),
-      hidden_name_(HiddenFile(dirname, micros)),
-      unhidden_name_(UnhiddenFile(dirname, micros)) {}
+      : file_(file),
+        fd_(fd),
+        offset_(0),
+        truncate_(-1),
+        hidden_name_(HiddenFile(dirname, micros)),
+        unhidden_name_(UnhiddenFile(dirname, micros)) {}
   ~SingleFile();
   void Write(io_context_t ctx, Block* b);
   int Outstanding() { return outstanding_.size(); }
@@ -59,12 +62,8 @@ class SingleFile {
     CHECK(outstanding_.erase(write) == 1);
     LOG(V2) << "File has " << outstanding_.size() << " remaining ops";
   }
-  void RequestClose() {
-    truncate_ = offset_;
-  }
-  bool Closable() {
-    return outstanding_.size() == 0 && truncate_ >= 0;
-  }
+  void RequestClose() { truncate_ = offset_; }
+  bool Closable() { return outstanding_.size() == 0 && truncate_ >= 0; }
   Error Close();
 
  private:
@@ -92,18 +91,13 @@ Error PWrite::Done(io_event* event) {
   return move(result);
 }
 
-SingleFile::~SingleFile() {
-  CHECK(fd_ == 0);
-}
+SingleFile::~SingleFile() { CHECK(fd_ == 0); }
 
 void SingleFile::Write(io_context_t ctx, Block* b) {
   auto data = b->Data();
   PWrite* write = new PWrite(b, this);
   iocb* cb = &write->cb;
-  io_prep_pwrite(
-      cb, fd_,
-      const_cast<char*>(data.data()),
-      data.size(), offset_);
+  io_prep_pwrite(cb, fd_, const_cast<char*>(data.data()), data.size(), offset_);
   offset_ += data.size();
   cb->data = reinterpret_cast<void*>(write);
   outstanding_.insert(write);
@@ -120,7 +114,7 @@ void SingleFile::Write(io_context_t ctx, Block* b) {
 Error SingleFile::Close() {
   CHECK(Closable());
   LOG(INFO) << "Closing " << hidden_name_ << " (" << fd_ << "), truncating to "
-      << (truncate_ >> 20) << "MB and moving to " << unhidden_name_;
+            << (truncate_ >> 20) << "MB and moving to " << unhidden_name_;
   LOG_IF_ERROR(Errno(ftruncate(fd_, truncate_) >= 0), "ftruncate");
   RETURN_IF_ERROR(Errno(close(fd_) >= 0), "close");
   fd_ = 0;
@@ -133,20 +127,22 @@ Error SingleFile::Close() {
 }  // namespace io
 
 Output::Output(int aiops, int64_t initial_file_size)
-    : ctx_(NULL), max_ops_(aiops),
-      initial_file_size_(initial_file_size), current_(NULL) {
+    : ctx_(NULL),
+      max_ops_(aiops),
+      initial_file_size_(initial_file_size),
+      current_(NULL) {
   CHECK_SUCCESS(SetUp());
 }
 
-Output::~Output() {
-  CHECK_SUCCESS(Flush());
-}
+Output::~Output() { CHECK_SUCCESS(Flush()); }
 
 Error Output::SetUp() {
   int ret;
   for (int i = 0; i < 10; i++) {
     ret = io_setup(max_ops_, &ctx_);
-    if (ret != -EAGAIN) { break; }
+    if (ret != -EAGAIN) {
+      break;
+    }
     SleepForSeconds(1);
     LOG(V1) << "io_setup retrying";
   }
@@ -182,17 +178,20 @@ Error Output::Rotate(const string& dirname, int64_t micros) {
     current_ = NULL;
   }
   string name = HiddenFile(dirname, micros);
-  int fd = open(name.c_str(), O_CREAT|O_WRONLY|O_DSYNC|O_DIRECT, 0600);
+  int fd = open(name.c_str(), O_CREAT | O_WRONLY | O_DSYNC | O_DIRECT, 0600);
   LOG(INFO) << "Opening packet file " << name << ": " << fd;
   RETURN_IF_ERROR(Errno(fd > 0), "open");
-  LOG_IF_ERROR(Errno(0 <= fallocate(fd, 0, 0, initial_file_size_)), "fallocate");
+  LOG_IF_ERROR(Errno(0 <= fallocate(fd, 0, 0, initial_file_size_)),
+               "fallocate");
   current_ = new io::SingleFile(this, dirname, micros, fd);
   files_.insert(current_);
   return SUCCESS;
 }
 
 Error Output::Flush() {
-  if (current_ == NULL) { return SUCCESS; }
+  if (current_ == NULL) {
+    return SUCCESS;
+  }
   current_->RequestClose();
   current_ = NULL;
   while (files_.size()) {

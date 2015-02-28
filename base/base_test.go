fd4ed9c6fdca4064999b2dabbdc19d4d5a637c83
openloop <sam.barranco@gmail.com>
2015-02-27 17:37:08 -0700

    Add base tests.

--- base/base_test.go
+++ base/base_test.go
@@ -15,11 +15,85 @@
 package base
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
+	"time"
 
 	"golang.org/x/net/context"
 )
+
+var ctx = context.Background()
+
+func testPacketData(t *testing.T) []*Packet {
+	var ci = []struct {
+		Timestamp     time.Time
+		CaptureLength int
+		Length        int
+	}{
+		{time.Unix(123, 123), 3, 3},
+		{time.Unix(456, 456), 3, 3},
+		{time.Unix(789, 789), 3, 3},
+	}
+
+	out := []*Packet{&Packet{[]byte{1, 2, 3}, ci[0]},
+		&Packet{[]byte{4, 5, 6}, ci[1]},
+		&Packet{[]byte{7, 8, 9}, ci[2]}}
+	return out
+}
+
+func comparePacketChans(t *testing.T, want, got *PacketChan) {
+	for {
+		p1, ok1 := <-want.Receive()
+		p2, ok2 := <-got.Receive()
+		if !ok1 || !ok2 {
+			if ok1 != ok2 {
+				t.Errorf("missing packet:\nwant:%v\ngot:%v\n", p1, p2)
+			}
+			break
+		}
+		if p1 != p2 {
+			t.Errorf("wrong packet\nwant:%v\ngot:%v\n", p1, p2)
+		}
+	}
+}
+
+func TestConcatPacketChans(t *testing.T) {
+	packets := testPacketData(t)
+	inputs := make(chan *PacketChan, 2)
+	one := NewPacketChan(100)
+	two := NewPacketChan(100)
+	one.Send(packets[0])
+	two.Send(packets[1])
+	one.Close(nil)
+	two.Close(nil)
+	inputs <- one
+	inputs <- two
+	close(inputs)
+	got := ConcatPacketChans(ctx, inputs)
+	want := NewPacketChan(3)
+	want.Send(packets[0])
+	want.Send(packets[1])
+	want.Close(nil)
+	comparePacketChans(t, want, got)
+}
+
+func TestMergePacketChans(t *testing.T) {
+	packets := testPacketData(t)
+	one := NewPacketChan(100)
+	two := NewPacketChan(100)
+	inputs := []*PacketChan{one, two}
+	one.Send(packets[1])
+	two.Send(packets[0])
+	one.Close(nil)
+	two.Close(nil)
+	got := MergePacketChans(ctx, inputs)
+	want := NewPacketChan(100)
+	want.Send(packets[0])
+	want.Send(packets[1])
+	want.Close(nil)
+	comparePacketChans(t, want, got)
+}
 
 func TestUnion(t *testing.T) {
 	for _, test := range []struct {
@@ -47,7 +121,8 @@ func TestUnion(t *testing.T) {
 		}
 	}
 }
-func Testintersect(t *testing.T) {
+
+func TestIntersect(t *testing.T) {
 	for _, test := range []struct {
 		a, b, want Positions
 	}{
@@ -76,6 +151,26 @@ func Testintersect(t *testing.T) {
 		if !reflect.DeepEqual(got, test.want) {
 			t.Errorf("nope:\n   a: %v\n   b: %v\n got: %v\nwant: %v", test.a, test.b, got, test.want)
 		}
+	}
+}
+
+func TestPacketsToFile(t *testing.T) {
+	var out bytes.Buffer
+	packets := testPacketData(t)
+	pc := NewPacketChan(100)
+	pc.Send(packets[0])
+	pc.Close(nil)
+	want := []byte{
+		0xd4, 0xc3, 0xb2, 0xa1, 0x02, 0x00, 0x04, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00,
+		0x7b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x03, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00,
+		0x01, 0x02, 0x03,
+	}
+	PacketsToFile(pc, &out)
+	if got := out.Bytes(); !bytes.Equal(want, got) {
+		t.Errorf("wrong packets:\nwant: %+v\ngot:  %+v", want, got)
 	}
 }
 
